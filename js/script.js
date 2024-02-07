c0f83e64bd4c6017e62d49798ecafbae38ba17b1
Joachim <jhertaas@gmail.com>
2024-02-07 11:10:10 +0100

    table and orienting

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-
 //Values
-const pricePerStudent = 27;
-const yearlyPrice = 180000;
-const startUpPrice = 150000;
+const pricePerStudent = parseInt(
+  document.getElementById("perStudent").innerHTML
+);
+
+const yearlyPrice = parseInt(
+  document.getElementById("yearlyPayment").innerHTML.replace(/\s/g, "")
+);
+
+const startUpPrice = parseInt(
+  document.getElementById("firstTimePayment").innerHTML.replace(/\s/g, "")
+);
+
 const container = document.getElementById("container");
 
 // Regular expression to match only digits (0-9)
@@ -12,24 +20,26 @@ const digitRegex = /^\d+$/;
 const displayContainer = document.createElement("p");
 container.appendChild(displayContainer);
 displayContainer.id = "displaySum";
-displayContainer.innerHTML = yearlyPrice;
+displayContainer.innerHTML = yearlyPrice.toLocaleString();
 
 //Function that calculates the total price which is (amountOfStudents * 27) + 180000
 function dynamicDisplay() {
-  let amountOfStudents = document.getElementById('students').value;
+  let amountOfStudents = document.getElementById("students").value;
+  //Ignores anything but numbers (should do so)
+  amountOfStudents = amountOfStudents.replace(/\D/g, "");
 
   //Even though the input is empty print the base startup price
-  if (amountOfStudents === '') {
-    displayContainer.innerText = yearlyPrice
+  if (amountOfStudents === "" || amountOfStudents === 0) {
+    displayContainer.innerText = yearlyPrice.toLocaleString();
   } else {
     //Using ternary operator to check if input is valid
     const checkError = digitRegex.test(amountOfStudents)
-    ? (amountOfStudents * pricePerStudent) + yearlyPrice
-    : "Please enter a valid number of students (digits only)";
-    
-    displayContainer.innerText = checkError
+      ? (amountOfStudents * pricePerStudent + yearlyPrice).toLocaleString()
+      : "Please enter a valid number of students (digits only)";
+
+    displayContainer.innerText = checkError;
   }
 }
 
 //Runs my function everytime anything changes within the input field
-document.getElementById("students").addEventListener('input', dynamicDisplay)
+document.getElementById("students").addEventListener("input", dynamicDisplay);
