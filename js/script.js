aab2f0c80d8010fd4f4774d516ab13352503028a
Joachim <jhertaas@gmail.com>
2024-02-09 10:53:17 +0100

    touches

--- js/script.js
+++ js/script.js
@@ -7,16 +7,19 @@ const yearlyPrice = parseInt(
   document.getElementById("yearlyPayment").innerHTML.replace(/\s/g, "")
 );
 
-const calContainer = document.getElementById("calContainer");
+const displayContainer = document.getElementById('displayContainer')
+
+//Values made for less repetition
+const yearlyString = `Pris per år: ${yearlyPrice.toLocaleString()} nok`
 
 // Regular expression to match only digits (0-9)
 const digitRegex = /^\d+$/;
 
 //Creating the display for the total cost
-const displayContainer = document.createElement("p");
-displayContainer.id = "displayCost";
-displayContainer.innerHTML = yearlyPrice.toLocaleString();
-calContainer.appendChild(displayContainer);
+const displayTag = document.createElement("p");
+displayTag.id = "displayTag";
+displayTag.innerHTML = yearlyString
+displayContainer.appendChild(displayTag);
 
 //Function that calculates the total price which is (amountOfStudents * 27) + 180000
 function dynamicDisplay() {
@@ -25,12 +28,12 @@ function dynamicDisplay() {
   //Using two ternary operators first to check if the field empty then if the input is valid
   const checkInput =
     amountOfStudents === ""
-      ? yearlyPrice.toLocaleString()
+      ? yearlyString
       : digitRegex.test(amountOfStudents)
-      ? (amountOfStudents * pricePerStudent + yearlyPrice).toLocaleString()
+      ? ( `Pris per år: ${(amountOfStudents * pricePerStudent + yearlyPrice).toLocaleString()} nok`)
       : "Venligst bare tall";
 
-  displayContainer.innerText = checkInput;
+  displayTag.innerText = checkInput;
 }
 
 //Blocks the input of anything but numbers 0-9, backspace, left arrow and right arrow
